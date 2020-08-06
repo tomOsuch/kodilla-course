36c5c2e32a1853dc640685947da6c14280e21339
tosuch <tomasz.osuch@gmail.com>
2020-08-06 02:32:19 +0200

    6.2 What is TDD

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,8 +1,26 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
 
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+
+        Calculator calculator = new Calculator();
+        
+        int resultAdd = calculator.add(2,1);
+        int resultSubtract = calculator.subtract(2,1);
+
+        if (resultAdd == 3) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error");
+        }
+
+        if (resultSubtract == 1) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error");
+        }
     }
 }
