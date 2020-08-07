7eb5f5b889446b662912ab7ea08e0976a97ff87a
tosuch <tomasz.osuch@gmail.com>
2020-08-08 01:25:51 +0200

    6.3 first JUnit Add

--- kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
@@ -1,2 +1,21 @@
-package com.kodilla.testing.collection;public class OddNumbersExterminator {
+package com.kodilla.testing.collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class OddNumbersExterminator {
+
+    public List<Integer> exterminate(List<Integer> numbers) {
+        List<Integer> numbersOdd = new ArrayList<>();
+        if (numbers != null){
+            for (Integer number : numbers) {
+                if (number % 2 != 0) {
+                    numbersOdd.add(number);
+                }
+            }
+        } else {
+            numbersOdd = null;
+        }
+        return numbersOdd;
+    }
 }
