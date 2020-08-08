db2fbf475b0ef6dcc45c456b2efcb23012bbaff1
tosuch <tomasz.osuch@gmail.com>
2020-08-08 12:16:23 +0200

    6.4 JUnit test TDD

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/shape/Square.java
@@ -0,0 +1,22 @@
+package com.kodilla.testing.shape;
+
+public class Square implements Shape {
+
+    private String shapeName;
+    private String field;
+
+    public Square(String shapeName, String field) {
+        this.shapeName = shapeName;
+        this.field = field;
+    }
+
+    @Override
+    public void getShapeName() {
+
+    }
+
+    @Override
+    public void getField() {
+
+    }
+}
