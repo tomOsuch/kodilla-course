67d68682fe3d467c66a5a8c29b33b92ffab1ede8
tosuch <tomasz.osuch@gmail.com>
2020-08-12 22:28:15 +0200

    6.7 - first test method ForumStatisticCalc

--- kodilla-testing/src/main/java/com/kodilla/testing/forum/statistics/ForumStatisticCalc.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/forum/statistics/ForumStatisticCalc.java
@@ -1,10 +1,67 @@
 package com.kodilla.testing.forum.statistics;
 
+
 import java.util.List;
 
 public class ForumStatisticCalc {
 
-    public List<Double> calculateAdvStatistics(Statistics statistics) {
-        return null;
+    private double userCount;
+    private double postsCount;
+    private double commentsCount;
+    private double averagePostsOfUserCount;
+    private double averageCommentsOfUserCount;
+    private double averageCommentsOfPostCount;
+
+    public double getUserCount() {
+        return userCount;
     }
+
+    public void setUserCount(double userCount) {
+        this.userCount = userCount;
+    }
+
+    public double getPostsCount() {
+        return postsCount;
+    }
+
+    public void setPostsCount(double postsCount) {
+        this.postsCount = postsCount;
+    }
+
+    public double getCommentsCount() {
+        return commentsCount;
+    }
+
+    public void setCommentsCount(double commentsCount) {
+        this.commentsCount = commentsCount;
+    }
+
+    public double getAveragePostsOfUserCount() {
+        return averagePostsOfUserCount;
+    }
+
+    public void setAveragePostsOfUserCount(double averagePostsOfUserCount) {
+        this.averagePostsOfUserCount = averagePostsOfUserCount;
+    }
+
+    public double getAverageCommentsOfUserCount() {
+        return averageCommentsOfUserCount;
+    }
+
+    public void setAverageCommentsOfUserCount(double averageCommentsOfUserCount) {
+        this.averageCommentsOfUserCount = averageCommentsOfUserCount;
+    }
+
+    public double getAverageCommentsOfPostCount() {
+        return averageCommentsOfPostCount;
+    }
+
+    public void setAverageCommentsOfPostCount(double averageCommentsOfPostCount) {
+        this.averageCommentsOfPostCount = averageCommentsOfPostCount;
+    }
+
+    public int calculateAdvStatisticsUserList(Statistics statistics) {
+        return statistics.userName().size();
+    }
+
 }
--- kodilla-testing/src/test/java/com/kodilla/testing/forum/statistics/ForumStatisticCalcTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/forum/statistics/ForumStatisticCalcTestSuite.java
@@ -3,13 +3,55 @@ package com.kodilla.testing.forum.statistics;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 
+
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+
 public class ForumStatisticCalcTestSuite {
 
     @Mock
-    private Statistics statistics;
+    private Statistics statisticsMock;
+
+    private List<String> generateListOfNData(int dataQuantity) {
+        List<String> resultList = new ArrayList<>();
+        for (int n = 1; n <= dataQuantity; n++){
+            String userName = "Jan_Test_" + n;
+            resultList.add(userName);
+        }
+        return resultList;
+    }
 
     @Test
-    void testCalculateAdvStatistics() {
+    void testCalculateAdvStatistics0User() {
+        //Given
+        ForumStatisticCalc statisticCalc = new ForumStatisticCalc();
+        List<String> resultUserList = generateListOfNData(0);
+         when(statisticsMock.userName()).thenReturn(resultUserList);
+        //When
+        int theUserListSize = statisticCalc.calculateAdvStatisticsUserList(statisticsMock);
+        //Then
+        assertEquals(0, theUserListSize);
 
     }
+
+    @Test
+    void testCalculateAdvStatistics100User() {
+        //Given
+        ForumStatisticCalc statisticCalc = new ForumStatisticCalc();
+        List<String> resultUserList = generateListOfNData(100);
+        when(statisticsMock.userName()).thenReturn(resultUserList);
+        //When
+        int theUserListSize = statisticCalc.calculateAdvStatisticsUserList(statisticsMock);
+        //Then
+        assertEquals(100, theUserListSize);
+
+    }
+
+
 }
