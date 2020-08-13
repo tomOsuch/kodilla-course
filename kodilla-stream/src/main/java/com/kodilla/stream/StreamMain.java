99cca1b21bf254a37a7e928a633a23e3b70676ef
tosuch <tomasz.osuch@gmail.com>
2020-08-14 01:09:27 +0200

    7 - Stream w kolekcjach update

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,8 +1,14 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.book.Book;
+import com.kodilla.stream.book.BookDirectory;
 import com.kodilla.stream.lambda.ExpressionExecutor;
 import com.kodilla.stream.reference.FunctionalCalculator;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+
 
 public class StreamMain {
 
@@ -24,9 +30,20 @@ public class StreamMain {
         System.out.println("Beautify words");
         PoemBeautifier poemBeautifier = new PoemBeautifier();
 
-        poemBeautifier.beautify("test doklejenia wyrazu: ", (a) -> "ABC" + a + "ABC");
+        poemBeautifier.beautify("test doklejenia wyrazu: ", (a) -> ("ABC" + a + "ABC"));
         poemBeautifier.beautify("upiększanie tekstu działa", (a) -> a.toUpperCase());
         poemBeautifier.beautify("Konkatenacja ", (a) -> a.concat(" Działa"));
         poemBeautifier.beautify("2.0", (a) -> a.replace(".", ","));
+
+        BookDirectory theBookDirectory = new BookDirectory();
+
+        Map<String, Book> theResultMapOfBooks = theBookDirectory.getList().stream()
+                .filter(book -> book.getYearOfPublication() > 2005)
+                .collect(Collectors.toMap(Book::getSignature, book -> book));
+
+        System.out.println("# elements: " + theResultMapOfBooks.size());
+        theResultMapOfBooks.entrySet().stream()
+                .map(entry -> entry.getKey() + ": " + entry.getValue())
+                .forEach(System.out::println);
     }
 }
