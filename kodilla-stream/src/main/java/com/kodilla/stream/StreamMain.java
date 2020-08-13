f79722b1c83b8b30aca3c079498ebfde7876fb52
tosuch <tomasz.osuch@gmail.com>
2020-08-13 22:22:32 +0200

    7 Stream - first commit update

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,17 +1,11 @@
 package com.kodilla.stream;
 
-import com.kodilla.stream.lambda.ExecuteSaySomething;
 import com.kodilla.stream.lambda.ExpressionExecutor;
-import com.kodilla.stream.lambda.Processor;
-import com.kodilla.stream.lambda.SaySomething;
 import com.kodilla.stream.reference.FunctionalCalculator;
 
 public class StreamMain {
 
     public static void main(String[] args) {
-        Processor processor = new Processor();
-        processor.execute(() -> System.out.println("This is an example text."));
-
         ExpressionExecutor expressionExecutor = new ExpressionExecutor();
 
         System.out.println("Calculating expressions with lambdas");
--- kodilla-stream/src/main/java/com/kodilla/stream/lambda/MathExpression.java
+++
@@ -1,6 +0,0 @@
-package com.kodilla.stream.lambda;
-
-public interface MathExpression {
-
-    double calculateExpression(double a, double b);
-}
