80d3faa152de6a4f600f1b96be53821fe9215034
tosuch <tomasz.osuch@gmail.com>
2020-08-15 00:59:30 +0200

    7. IntStream

---
+++ kodilla-stream/src/test/java/com/kodilla/stream/book/BookTestSuite.java
@@ -0,0 +1,26 @@
+package com.kodilla.stream.book;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.stream.IntStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BookTestSuite {
+
+    @Test
+    void getTestListUsingFor() {
+        //Given
+        BookDirectory bookDirectory = new BookDirectory();
+
+        //When
+        List<Book> books = bookDirectory.getList();
+
+        //Then
+        long numberOfBooksPublicatedAfter2007 = IntStream.range(0, books.size())
+                .filter(n -> books.get(n).getYearOfPublication() > 2007)
+                .count();
+        assertEquals(3, numberOfBooksPublicatedAfter2007);
+    }
+}
