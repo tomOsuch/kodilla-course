5651c8704f7563027cbbf9bd9363549f95cf1213
tosuch <tomasz.osuch@gmail.com>
2020-08-19 15:39:39 +0200

    8.2 - ArithmeticException try-catch-finally - update

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
@@ -4,26 +4,27 @@ package com.kodilla.exception.test;
 public class FirstChallenge {
 
     public double divide(double a, double b) throws ArithmeticException {
-        double resultTry = 0;
-        try {
-            if (b == 0) throw new ArithmeticException();
-            resultTry = a / b;
-        } catch (ArithmeticException e) {
-            System.out.println("Dzielenie przez zero!");
-        } finally {
-            System.out.println("Działa try-catch-finally");;
+        if (b == 0){
+            throw new ArithmeticException();
         }
-        return resultTry;
+        return a / b;
     }
 
-
+    /**
+     * This main can throw an ArithmeticException!!!
+     * @param args
+     */
     public static void main(String[] args) {
 
         FirstChallenge firstChallenge = new FirstChallenge();
 
-        double result = firstChallenge.divide(3, 0);
-
-        System.out.println(result);
-
+        try {
+            double result = firstChallenge.divide(3, 0);
+            System.out.println(result);
+        } catch (ArithmeticException e) {
+            System.out.println("Dzielenie przez zero!");
+        } finally {
+            System.out.println("Działa try-catch-finally");
+        }
     }
 }
