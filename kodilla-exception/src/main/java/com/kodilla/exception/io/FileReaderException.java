d7151b70abcab12f8390b353c9eb6a53d66235ec
tosuch <tomasz.osuch@gmail.com>
2020-08-19 16:02:19 +0200

    8. Throw and Throws - update test

--- kodilla-exception/src/main/java/com/kodilla/exception/io/FileReaderException.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/io/FileReaderException.java
@@ -2,5 +2,5 @@ package com.kodilla.exception.io;
 
 import java.io.IOException;
 
-public class FileReaderExceptio extends Exception {
+public class FileReaderException extends Exception {
 }
--- kodilla-exception/src/main/java/com/kodilla/exception/main/ExceptionModuleRunner.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/main/ExceptionModuleRunner.java
@@ -1,18 +1,15 @@
 package com.kodilla.exception.main;
 
 import com.kodilla.exception.io.FileReader;
-import com.kodilla.exception.io.FileReaderExceptio;
-import com.kodilla.exception.io.FileReaderWithoutHandling;
-
-import java.nio.file.FileAlreadyExistsException;
+import com.kodilla.exception.io.FileReaderException;
 
 public class ExceptionModuleRunner {
 
     public static void main(String[] args) {
         FileReader fileReader = new FileReader();
         try {
-            fileReader.readFile();
-        } catch (FileReaderExceptio e) {
+            fileReader.readFile("names.txt");
+        } catch (FileReaderException e) {
             System.out.println("Problem while reading a file!");
         }
 
