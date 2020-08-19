3fcc6507ea604acdf1d9c792be183aab01ae73f5
tosuch <tomasz.osuch@gmail.com>
2020-08-19 15:36:00 +0200

    8. Throw and Throws

--- kodilla-exception/src/main/java/com/kodilla/exception/main/ExceptionModuleRunner.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/main/ExceptionModuleRunner.java
@@ -1,11 +1,20 @@
 package com.kodilla.exception.main;
 
 import com.kodilla.exception.io.FileReader;
+import com.kodilla.exception.io.FileReaderExceptio;
+import com.kodilla.exception.io.FileReaderWithoutHandling;
+
+import java.nio.file.FileAlreadyExistsException;
 
 public class ExceptionModuleRunner {
 
     public static void main(String[] args) {
         FileReader fileReader = new FileReader();
-        fileReader.readFile();
+        try {
+            fileReader.readFile();
+        } catch (FileReaderExceptio e) {
+            System.out.println("Problem while reading a file!");
+        }
+
     }
 }
