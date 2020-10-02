3640667831718f6c662808cdad3fe54c3224a531
tosuch <tomasz.osuch@gmail.com>
2020-10-02 17:00:19 +0200

    14.1 - bean ForumUser

---
+++ kodilla-spring/src/test/java/com/kodilla/spring/forum/ForumUserTestSuite.java
@@ -0,0 +1,24 @@
+package com.kodilla.spring.forum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class ForumUserTestSuite {
+
+    @Test
+    void testGetUsername() {
+        //Give
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext("com.kodilla.spring");
+        ForumUser user = new ForumUser();
+        //When
+        String userResult = user.getUsername();
+        //Then
+        assertEquals(userResult, "John Smith");
+    }
+}
