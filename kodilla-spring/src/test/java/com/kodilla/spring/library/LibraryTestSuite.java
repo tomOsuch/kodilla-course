983c107f640e748180f41b1d65e40d1641371d70
tosuch <tomasz.osuch@gmail.com>
2020-10-04 10:15:12 +0200

    14.3 - Konfiguracja za pomoca klas

--- kodilla-spring/src/test/java/com/kodilla/spring/library/LibraryTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/library/LibraryTestSuite.java
@@ -5,6 +5,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.util.Arrays;
+
 @SpringBootTest
 public class LibraryTestSuite {
 
@@ -26,5 +28,17 @@ public class LibraryTestSuite {
         //When
         library.saveToDb();
         //Then
+    }
+
+    @Test
+    void testContext() {
+        //Given
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext("com.kodilla.spring");
+        //When & Then
+        System.out.println("<< -- Beans list: -- >>");
+        Arrays.stream(context.getBeanDefinitionNames())
+                .forEach(System.out::println);
+        System.out.println("<< -- Beans list -- >>");
     }
 }
