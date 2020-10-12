ca88f4a17e6db5d0f136b8f072b652c75928060b
tosuch <tomasz.osuch@gmail.com>
2020-10-12 16:30:20 +0200

    15_Prototype

---
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/prototype/Board.java
@@ -0,0 +1,54 @@
+package com.kodilla.patterns.prototype;
+
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+public class Board extends Prototype<Board> {
+
+    private String name;
+    private final Set<TasksList> lists = new HashSet<>();
+
+    public Board(String name) {
+        this.name = name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Set<TasksList> getLists() {
+        return lists;
+    }
+
+    @Override
+    public String toString() {
+        String s = "Board [" + name + "]\n";
+        for (TasksList list : lists) {
+            s = s + list.toString() + "\n";
+        }
+        return s;
+    }
+
+    public Board shallowCopy() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Board board = (Board) o;
+        return Objects.equals(name, board.name) &&
+                Objects.equals(lists, board.lists);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, lists);
+    }
+}
---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/prototype/BoardTestSuite.java
@@ -0,0 +1,43 @@
+package com.kodilla.patterns.prototype;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.stream.IntStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BoardTestSuite {
+
+    @Test
+    void testToString() {
+        //given
+        //creating the TasksList for todos
+        TasksList listToDo = new TasksList("To Do Tasks");
+        IntStream.iterate(1, n -> n + 1)
+                .limit(10)
+                .forEach(n -> listToDo.getTasks().add(new Task("To Do Task number " + n)));
+
+        //creating the TaskList for tasks in progress
+        TasksList listInProgress = new TasksList("In Progress Tasks");
+        IntStream.iterate(1, n -> n + 1)
+                .limit(10)
+                .forEach(n -> listInProgress.getTasks().add(new Task("In Progress Task number " + n)));
+
+        //creating the TaskList for done tasks
+        TasksList listDone = new TasksList("Done Tasks");
+        IntStream.iterate(1, n -> n + 1)
+                .limit(10)
+                .forEach(n -> listDone.getTasks().add(new Task("Done Task number " + n)));
+
+        //creating the board and assigning the lists
+        Board board = new Board("Project number 1");
+        board.getLists().add(listToDo);
+        board.getLists().add(listInProgress);
+        board.getLists().add(listDone);
+        System.out.println(board);
+
+        //When
+        //Then
+        assertEquals(3, board.getLists().size());
+    }
+}
