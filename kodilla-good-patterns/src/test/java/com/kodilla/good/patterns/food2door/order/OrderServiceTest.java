37fc9c91c296541f34aac6f8fcb832480cb069f5
tosuch <tomasz.osuch@gmail.com>
2020-10-12 20:32:00 +0200

    13_4_food2door - update test

--- kodilla-good-patterns/src/test/java/com/kodilla/good/patterns/food2door/order/OrderServiceTest.java
+++ kodilla-good-patterns/src/test/java/com/kodilla/good/patterns/food2door/order/OrderServiceTest.java
@@ -5,59 +5,57 @@ import com.kodilla.good.patterns.food2door.components.model.provider.Provider;
 import com.kodilla.good.patterns.food2door.components.order.OrderException;
 import com.kodilla.good.patterns.food2door.components.order.OrderServiceImpl;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
 import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 
 public class OrderServiceTest {
 
-    private Map<Product, Integer> createOrder() {
-        Provider firstProviderMock = mock(Provider.class);
-        Provider secondProviderMock = mock(Provider.class);
-        Provider thirdProviderMock = mock(Provider.class);
+
+    static Provider successProviderMock = mock(Provider.class);
+    static Provider failedProviderMock = mock(Provider.class);
+    OrderServiceImpl orderService = new OrderServiceImpl();
+
+    @BeforeAll
+    public static void initsMock() {
+        when(successProviderMock.process(any())).thenReturn(true);
+        when(failedProviderMock.process(any())).thenReturn(false);
+    }
+
+    private Map<Product, Integer> createOrder(Provider provider) {
 
         return Map.of(
-                new Product("Chleb bez glutenowy", firstProviderMock, new BigDecimal("5.99")), 2,
-                new Product("Mleko sojowe", firstProviderMock, new BigDecimal("4.99")), 1,
-                new Product("Krewetki", secondProviderMock, new BigDecimal("10.50")), 3,
-                new Product("Kalmary", thirdProviderMock, new BigDecimal("10.50")), 3
+                new Product("Chleb bez glutenowy", successProviderMock, new BigDecimal("5.99")), 2,
+                new Product("Mleko sojowe", successProviderMock, new BigDecimal("4.99")), 1,
+                new Product("Krewetki", provider, new BigDecimal("10.50")), 3
+
         );
     }
 
     @Test
     void testOrderTrue() throws OrderException {
         //Give
-        Provider firstProviderMock = mock(Provider.class);
-        Provider secondProviderMock = mock(Provider.class);
-        Provider thirdProviderMock = mock(Provider.class);
-        OrderServiceImpl orderService = new OrderServiceImpl();
-        when(firstProviderMock.process(createOrder())).thenReturn(true);
-        when(secondProviderMock.process(createOrder())).thenReturn(true);
-        when(thirdProviderMock.process(createOrder())).thenReturn(true);
+        Map<Product, Integer> mapOrder = createOrder(successProviderMock);
         //When
 
         //Then
-        Assertions.assertDoesNotThrow(() -> orderService.order(createOrder()));
+        Assertions.assertDoesNotThrow(() -> orderService.order(mapOrder));
     }
 
     @Test
     void testOrderFalse() throws OrderException {
-        Provider firstProviderMock = mock(Provider.class);
-        Provider secondProviderMock = mock(Provider.class);
-        Provider thirdProviderMock = mock(Provider.class);
-        OrderServiceImpl orderService = new OrderServiceImpl();
-        when(firstProviderMock.process(createOrder())).thenReturn(true);
-        when(secondProviderMock.process(createOrder())).thenReturn(true);
-        when(thirdProviderMock.process(createOrder())).thenReturn(false);
+        //Given
+        Map<Product, Integer> mapOrder = createOrder(failedProviderMock);
         //When
 
         //Then
-        Assertions.assertThrows(OrderException.class, () -> orderService.order(createOrder()));
+        Assertions.assertThrows(OrderException.class, () -> orderService.order(mapOrder));
     }
 }
