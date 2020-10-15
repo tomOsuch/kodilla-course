65c9a8f743c872c97664de232e7f6e95432713ec
tosuch <tomasz.osuch@gmail.com>
2020-10-15 11:06:24 +0200

    task_15_3_strategy - socialMedia

---
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/strategy/social/User.java
@@ -0,0 +1,23 @@
+package com.kodilla.patterns.strategy.social;
+
+public class User {
+
+    private final String username;
+    protected SocialPublisher socialPublisher;
+
+    public User(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setSocialPublisher(SocialPublisher socialPublisher) {
+        this.socialPublisher = socialPublisher;
+    }
+
+    public String sharePost() {
+        return socialPublisher.share();
+    }
+}
