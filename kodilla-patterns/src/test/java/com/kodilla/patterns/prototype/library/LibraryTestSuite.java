0c807dad6da18c0b460ecc6e852f1da75420c0ec
tosuch <tomasz.osuch@gmail.com>
2020-10-16 10:47:21 +0200

    15_2_prototype - update test

--- kodilla-patterns/src/test/java/com/kodilla/patterns/prototype/library/LibraryTestSuite.java
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/prototype/library/LibraryTestSuite.java
@@ -1,45 +1,51 @@
 package com.kodilla.patterns.prototype.library;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
 import java.util.stream.IntStream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 
 public class LibraryTestSuite {
 
+    private static final Library library = new Library("Koszykowa");
+
+
     @Test
-    void testGetBooks() {
+    void testShallowCopy() throws CloneNotSupportedException {
         //Given
-        Library library = new Library("Koszykowa");
         IntStream.iterate(1, n -> n = n + 1)
                 .limit(10)
                 .forEach(n -> library.getBooks().add(new Book("Title " + n, "Author " + n, LocalDate.now())));
+
         Library clonedLibrary = null;
-        try {
-            clonedLibrary = library.shallowCopy();
-            clonedLibrary.setName("Płytki Klon Koszykowa");
-        } catch (CloneNotSupportedException e) {
-            System.out.println(e);
+        clonedLibrary = library.shallowCopy();
+        clonedLibrary.setName("Płytki Klon Koszykowa");
+        //When
+        library.getBooks().removeAll(library.getBooks());
+        //Then
+        assertEquals(clonedLibrary.getBooks(), library.getBooks());
+    }
 
-
-        }
+    @Test
+    void testDeepCloned() throws CloneNotSupportedException {
+        //Given
+        IntStream.iterate(1, n -> n = n + 1)
+                .limit(10)
+                .forEach(n -> library.getBooks().add(new Book("Title " + n, "Author " + n, LocalDate.now())));
 
         Library deepClonedLibrary = null;
-        try {
-            deepClonedLibrary = library.deepCopy();
-            deepClonedLibrary.setName("Głęboki klon Koszykowa");
-        } catch (CloneNotSupportedException e) {
-            System.out.println(e);
-        }
+        deepClonedLibrary = library.deepCopy();
+        deepClonedLibrary.setName("Głęboki klon Koszykowa");
         //When
-
+        library.getBooks().removeAll(library.getBooks());
         //Then
-        assertEquals(10, library.books.size());
-        assertEquals(10, deepClonedLibrary.books.size());
-        assertEquals(10, clonedLibrary.books.size());
-
+        //assertEquals(10, library.getBooks().size());
+        assertNotEquals(deepClonedLibrary.getBooks(), library.getBooks());
     }
 }
