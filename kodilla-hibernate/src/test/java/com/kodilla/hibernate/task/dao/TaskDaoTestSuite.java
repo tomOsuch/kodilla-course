2e6c3bc4f6819d7e6ef96ab64d3e7f71e5abe326
tosuch <tomasz.osuch@gmail.com>
2020-10-27 23:33:19 +0100

    Master - HQL and NamedQuery

--- kodilla-hibernate/src/test/java/com/kodilla/hibernate/task/dao/TaskDaoTestSuite.java
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/task/dao/TaskDaoTestSuite.java
@@ -1,9 +1,15 @@
 package com.kodilla.hibernate.task.dao;
 
 import com.kodilla.hibernate.task.Task;
+import com.kodilla.hibernate.task.TaskFinancialDetails;
+import com.kodilla.hibernate.task.TaskList;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.math.BigDecimal;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -12,7 +18,15 @@ import static org.junit.jupiter.api.Assertions.*;
 class TaskDaoTestSuite {
     @Autowired
     private TaskDao taskDao;
+    @Autowired
+    private TaskListDao taskListDao;
     private static final String DESCRIPTION = "Test: Learn Hibernate";
+
+    @AfterEach
+    public void clean() {
+        taskDao.deleteAll();
+        taskListDao.deleteAll();
+    }
 
     @Test
     void testTaskDaoSave() {
@@ -43,5 +57,55 @@ class TaskDaoTestSuite {
 
         //CleanUp
         //taskDao.deleteById(id);
+    }
+
+    @Test
+    void testNamedQueries() {
+        //Given
+        Task task1 = new Task("Test: Study Hibernate", 3);
+        Task task2 = new Task("Test: Practice Named Queries", 6);
+        Task task3 = new Task("Test: Study native queries", 7);
+        Task task4 = new Task("Test: Makse some tests", 13);
+
+        TaskFinancialDetails tfd1 = new TaskFinancialDetails(new BigDecimal(5), false);
+        TaskFinancialDetails tfd2 = new TaskFinancialDetails(new BigDecimal(10), false);
+        TaskFinancialDetails tfd3 = new TaskFinancialDetails(new BigDecimal(20), false);
+        TaskFinancialDetails tfd4 = new TaskFinancialDetails(new BigDecimal(15), false);
+
+        task1.setTaskFinancialDetails(tfd1);
+        task2.setTaskFinancialDetails(tfd2);
+        task3.setTaskFinancialDetails(tfd3);
+        task4.setTaskFinancialDetails(tfd4);
+
+        TaskList taskList = new TaskList("TODO", "ToDo tasks");
+        taskList.getTasks().add(task1);
+        taskList.getTasks().add(task2);
+        taskList.getTasks().add(task3);
+        taskList.getTasks().add(task4);
+
+        task1.setTaskList(taskList);
+        task2.setTaskList(taskList);
+        task3.setTaskList(taskList);
+        task4.setTaskList(taskList);
+
+        taskListDao.save(taskList);
+        int id = taskList.getId();
+
+        //When
+        List<Task> longTasks = taskDao.retrieveLongTasks();
+        List<Task> shortTasks = taskDao.retrieveShortTasks();
+        //List<Task> enoughTimeTasks = taskDao.retrieveTasksWithEnoughTime();
+        List<Task> durationLongerThanTask = taskDao.retrieveTasksWithDurationLongerThan(6);
+
+        //Then
+        try {
+            assertEquals(1, longTasks.size());
+            assertEquals(3, shortTasks.size());
+            //assertEquals(3, enoughTimeTasks.size());
+            assertEquals(2, durationLongerThanTask.size());
+        } finally {
+            //CleanUp
+            taskListDao.deleteById(id);
+        }
     }
 }
