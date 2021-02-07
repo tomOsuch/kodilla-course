2094bdae9993c7a917cd545cc3a7cb3f13a52756
tosuch <tomasz.osuch@gmail.com>
2021-02-07 16:33:32 +0100

    task-31-2-facebook test update

--- kodilla-testing2/src/main/java/com/kodilla/testing2/facebook/FacebookTestingApp.java
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/facebook/FacebookTestingApp.java
@@ -8,28 +8,33 @@ import org.openqa.selenium.support.ui.Select;
 
 public class FacebookTestingApp {
 
+    public static final String XPATH_COOKIES = "//div[contains(@class, \"_9fiw\")]/button[2]";
+    public static final String XPATH_REGISTRATION_ACCOUNT = "//div[contains(@class, \"_6ltg\")]/a";
+    public static final String ID_BIRTH_DAY = "day";
+    public static final String ID_BIRTH_MONTH = "month";
+    public static final String ID_BIRTH_YEAR = "year";
+
     public static void main(String[] args) {
         WebDriver driver = WebDriverConfig.getDriver(WebDriverConfig.CHROME);
         driver.get("https://www.facebook.com");
 
-        WebElement acceptCookie = driver.findElement(By.id("u_0_h"));
+        WebElement acceptCookie = driver.findElement(By.xpath(XPATH_COOKIES));
         acceptCookie.click();
 
-        WebElement registration = driver.findElement(By.id("u_0_2"));
+        WebElement registration = driver.findElement(By.xpath(XPATH_REGISTRATION_ACCOUNT));
         registration.click();
 
-        WebElement birthDay = driver.findElement(By.id("day"));
+        WebElement birthDay = driver.findElement(By.id(ID_BIRTH_DAY));
         Select selectBirthDay = new Select(birthDay);
-        selectBirthDay.selectByIndex(18);
+        selectBirthDay.selectByValue("18");
 
-        WebElement birthMonth = driver.findElement(By.id("month"));
+        WebElement birthMonth = driver.findElement(By.id(ID_BIRTH_MONTH));
         Select selectBirthMonth = new Select(birthMonth);
-        selectBirthMonth.selectByIndex(2);
+        selectBirthMonth.selectByValue("2");
 
-        while (!driver.findElement(By.id("birthday_wrapper")).isDisplayed());
 
-        WebElement birthYear = driver.findElement(By.id("year"));
+        WebElement birthYear = driver.findElement(By.id(ID_BIRTH_YEAR));
         Select selectBirthYear = new Select(birthYear);
-        selectBirthYear.selectByIndex(20);
+        selectBirthYear.selectByValue("1988");
     }
 }
