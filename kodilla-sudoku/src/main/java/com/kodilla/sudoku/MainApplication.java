eeb1510215509e76ef7430f251e731fbb79201a0
tosuch <tomasz.osuch@gmail.com>
2021-02-09 17:22:03 +0100

    sudoku-project first commit

---
+++ kodilla-sudoku/src/main/java/com/kodilla/sudoku/MainApplication.java
@@ -0,0 +1,15 @@
+package com.kodilla.sudoku;
+
+public class MainApplication {
+
+    private static boolean gameFinished = false;
+
+    public static void main(String[] args) {
+
+
+        while (!gameFinished) {
+
+        }
+        System.out.println("see you");
+    }
+}
