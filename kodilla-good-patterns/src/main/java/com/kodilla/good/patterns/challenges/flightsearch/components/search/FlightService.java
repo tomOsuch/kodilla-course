f5f7fa2499f05e9eaa0e3f510030a692495ec0a0
Tomasz Osuch <tomasz.osuch@gmail.com>
2021-03-24 02:54:54 +0100

    master - update

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/flightsearch/components/search/FlightService.java
+++
@@ -1,9 +0,0 @@
-package com.kodilla.good.patterns.challenges.flightsearch.components.search;
-
-public interface FlightService {
-    void findFlightsFrom(String source);
-
-    void findFlightsTo(String destination);
-
-    void findFlightsWithInterstation(String source, String interstation, String destination);
-}
